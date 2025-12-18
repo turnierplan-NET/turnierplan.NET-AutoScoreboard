549b898adf434f4d8a8217bcbf3139233fd49d14
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-12-18 21:53:03 +0100

    Add ascii art

--- src/Turnierplan.AutoScoreboard/Program.cs
+++ src/Turnierplan.AutoScoreboard/Program.cs
@@ -1,3 +1,15 @@
+Console.WriteLine();
+Console.WriteLine( "  __                                                     ___                                        __");
+Console.WriteLine(@" /\ \__                        __                       /\_ \                                      /\ \__");
+Console.WriteLine(@" \ \ ,_\  __  __  _ __    ___ /\_\     __   _ __   _____\//\ \      __      ___         ___      __\ \ ,_\");
+Console.WriteLine(@"  \ \ \/ /\ \/\ \/\`'__\/' _ `\/\ \  /'__`\/\`'__\/\ '__`\\ \ \   /'__`\  /' _ `\     /' _ `\  /'__`\ \ \/");
+Console.WriteLine(@"   \ \ \_\ \ \_\ \ \ \/ /\ \/\ \ \ \/\  __/\ \ \/ \ \ \L\ \\_\ \_/\ \L\.\_/\ \/\ \  __/\ \/\ \/\  __/\ \ \_");
+Console.WriteLine(@"    \ \__\\ \____/\ \_\ \ \_\ \_\ \_\ \____\\ \_\  \ \ ,__//\____\ \__/.\_\ \_\ \_\/\_\ \_\ \_\ \____\\ \__\");
+Console.WriteLine(@"     \/__/ \/___/  \/_/  \/_/\/_/\/_/\/____/ \/_/   \ \ \/ \/____/\/__/\/_/\/_/\/_/\/_/\/_/\/_/\/____/ \/__/");
+Console.WriteLine(@"                                                     \ \_\");
+Console.WriteLine(@"                                                      \/_/   turnierplan.NET AutoScoreboard");
+Console.WriteLine();
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
